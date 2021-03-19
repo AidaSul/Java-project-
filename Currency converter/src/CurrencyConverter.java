20d56839d0b67d78d83a2ad6ea005af806aac3d9
AidaSul <aida.sultan@smu.ca>
2021-03-19 10:25:46 -0300

    new edits

--- Currency converter/src/CurrencyConverter.java
+++ Currency converter/src/CurrencyConverter.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CurrencyConverter {
 
@@ -9,12 +10,19 @@ public class CurrencyConverter {
         // we are going to map certain numbers to strings that will be our  codes
 
         //Add currency code
-        currencyCode.put(1, "BIR");
+        currencyCode.put(1, "BIR"); //If we pass in a key on 1 we will get a string back which is BIR
         currencyCode.put(2, "CAD");
         currencyCode.put(3, "Leon");
         currencyCode.put(4, "USD");
         currencyCode.put(5, "Euro");
         currencyCode.put(6, "Pound");
+
+        String fromCode, toCode;
+        double amount;
+
+        Scanner sc = new Scanner(System.in); // To get the content from user
+
+        System.out.println("Welcome to the currency converter"); 
 
           
 
