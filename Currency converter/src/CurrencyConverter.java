6ac4b0655f636880cb0714036ee4c9de40ff5a02
AidaSul <aida.sultan@smu.ca>
2021-03-19 10:08:37 -0300

    Currency converter project

---
+++ Currency converter/src/CurrencyConverter.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class CurrencyConverter {
+
+    public static void main(String[] args) {
+        //Hash maps maps a key to a value
+        HashMap<Integer, String> currencyCode = new HashMap<Integer, String>();
+        // this hashmap out key is an int and that value is a string
+        // we are going to map certain numbers to strings that will be our  codes
+
+        //Add currency code
+        currencyCode.put(1, "BIR");
+        currencyCode.put(2, "CAD");
+        currencyCode.put(3, "Leon");
+        currencyCode.put(4, "USD");
+        currencyCode.put(5, "Euro");
+        currencyCode.put(6, "Pound");
+
+          
+
+
+
+
+
+    }
+}
